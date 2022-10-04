c655e7137d7d7148d52521c09a6059cbb4619f45
Proyecto E6 <ticm062@gmail.com>
2022-10-04 11:34:36 -0500

    se agregan extracion de tabla y tratada por medio de leerexpla y modificacion de datos a leer.

--- system/libreria/views.py
+++ system/libreria/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import explanetas, userandpassword
+from .models import explanetas
 
 def Inicio(request):
     Explanetas = explanetas.objects.all()
-    return render(request, 'paginas/inicio.html')
+    return render(request, 'paginas/inicio.html', {'Explanetas': Explanetas})
 def Login (request):
-    Userandpassword = userandpassword.objects.all()
     return render(request, 'paginas/login.html')
 def Recuperarpass(request):
     return render(request, 'paginas/recuperarpass.html')
